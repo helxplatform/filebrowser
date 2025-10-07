0752367c692479afcfec63fc6068a18c155eef8e
Charles Bennett <cnbennett3@gmail.com>
2025-10-07 12:07:49 -0400

    Addiional changes for recycle bin.

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,44 @@
 FROM debian:bullseye-slim
 
-# Copy files and set permissions
-COPY filebrowser /bin/filebrowser
-RUN chmod +x /bin/filebrowser && cp /bin/filebrowser /filebrowser
+USER root
 
-COPY docker/common/ /tmp/common/
-COPY docker/debian/ /tmp/debian/
-RUN find /tmp/common /tmp/debian -type f -name "*.sh" -exec chmod +x {} \; && \
-  cp -r /tmp/common/* / && \
-  cp -r /tmp/debian/* /
+# Set filebrowser env vars:
+ENV FB_AUTH_METHOD="noauth"
+ENV FB_NOAUTH=true
+
+# Copy the filebrowser binary, scripts, and settings
+COPY --chmod=0755 filebrowser /
+COPY --chmod=0755 docker/debian/ /
+COPY --chmod=0755 docker/common/healthcheck.sh /
+COPY --chmod=0666 docker/common/defaults/settings.json /
 
 # Update package list and install required packages
-RUN apt-get update && \
-  DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && \
-  DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
-    ca-certificates \
-    media-types \
-    wget \
-    tini \
-    trash-cli
-
-# Libnss-ldap removed on bookworm, manually install from archive
-RUN wget https://ftp.debian.org/debian/pool/main/libn/libnss-ldap/libnss-ldap_265-6_amd64.deb && \
-  wget https://ftp.debian.org/debian/pool/main/o/openldap/libldap-2.4-2_2.4.47+dfsg-3+deb10u7_amd64.deb && \
-  apt-get install -y "./libldap-2.4-2_2.4.47+dfsg-3+deb10u7_amd64.deb" "./libnss-ldap_265-6_amd64.deb"
-
+RUN ls -l && \
+    apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+       ca-certificates \
+       media-types \
+       wget \
+       gnupg2 \
+       mailcap \
+       binutils \
+       libnss-ldap/bullseye \
+       ldap-utils \
+       nscd \
+       procps \
+       curl \
+       jq \
+       tini \
+       less \
+       vim \
+       trash-cli
 
 HEALTHCHECK --start-period=2s --interval=5s --timeout=3s CMD /healthcheck.sh || exit 1
 
-VOLUME /srv /config /database
+VOLUME /srv /database
 
 EXPOSE 80
 
 ENTRYPOINT [ "tini", "--", "/init.sh" ]
-CMD [ "/filebrowser", "--config", "/config/settings.json" ]
+CMD [ "/filebrowser", "--root=$ROOT_DIR", "--address=0.0.0.0", "--port=8080", "-d=$HOME/.filebrowser/filebrowser.db", "-c=/settings.json" ]
--- docker/debian/init.sh
+++ docker/debian/init.sh
@@ -2,54 +2,81 @@
 
 set -e
 
-# Backwards compatibility for old Docker image
-if [ -f "/.filebrowser.json" ]; then
-  ln -s /.filebrowser.json /config/settings.json
+DB_DIR_PATH="${HOME}/.filebrowser"
+DB_PATH="${DB_DIR_PATH}/filebrowser.db"
+CONFIG_PATH="/settings.json"
 
-  echo ""
-  echo "!!!!!!!!!!!!!!!!!!!!! IMPORTANT INFORMATION !!!!!!!!!!!!!!!!!!!!!"
-  echo "Symlinking /.filebrowser.json to /config/settings.json for backwards compatibility."
-  echo ""
-  echo "The volume mount configuration has changed in the latest release."
-  echo "Please rename .filebrowser.json to settings.json and mount the parent directory to /config".
-  echo "Read more on https://github.com/filebrowser/filebrowser/blob/master/docs/installation.md#docker"
-  echo ""
-  echo "This workaround will be removed in a future release."
-  echo ""
-fi
-
-# Backwards compatibility for old Docker image
-if [ -f "/database.db" ]; then
-  ln -s /database.db /database/filebrowser.db
-  
-  echo ""
-  echo "!!!!!!!!!!!!!!!!!!!!! IMPORTANT INFORMATION !!!!!!!!!!!!!!!!!!!!!"
-  echo ""
-  echo "The volume mount configuration has changed in the latest release."
-  echo "Please rename database.db to filebrowser.db and mount the parent directory to /database".
-  echo "Read more on https://github.com/filebrowser/filebrowser/blob/master/docs/installation.md#docker"
-  echo ""
-  echo "This workaround will be removed in a future release."
-  echo ""
-fi
-
-if [ ! -f "/database/filebrowser.db" ]; then
-  filebrowser config -d /database/filebrowser.db init
-  ln -s /database/filebrowser.db /filebrowser.db
-  filebrowser users add admin admin --perm.admin
-fi
-
-# Ensure configuration exists
-if [ ! -f "/config/settings.json" ]; then
-  cp -a /defaults/settings.json /config/settings.json
-fi
-
+SAFE_SOFTLINK_HELX="/helx-softlink.sh"
 SETUP_TRASH="/setup-trash.sh"
-if [ -x $SETUP_TRASH ]; then
-  echo "Running $SETUP_TRASH..."
-  "$SETUP_TRASH"
+
+
+echo "init.sh script starting . . ."
+
+# Set ROOT_DIR
+if [ -d /shared ]; then
+  export ROOT_DIR="/home/$USER"
 else
-  echo "$SETUP_TRASH not found or not executable, skipping..."
+  export ROOT_DIR="/home"
 fi
 
-exec "$@"
+# Set up softlink to postgresql data
+if [ -x "$SAFE_SOFTLINK_HELX" ]; then
+    echo "[init.sh]: Running $SAFE_SOFTLINK_HELX..."
+    "$SAFE_SOFTLINK_HELX"
+    echo "[init.sh]: $SAFE_SOFTLINK_HELX return code: [$?]"
+else
+    echo "[init.sh]: $SAFE_SOFTLINK_HELX not found or not executable, skipping..."
+fi
+
+# Ensure database directory exists
+if [ ! -d "$DB_DIR_PATH" ]; then
+    echo "[init.sh]: Creating Filebrowser database directory at $DB_DIR_PATH..."
+    mkdir -p "$DB_DIR_PATH"
+else
+    echo "[init.sh]: $DB_DIR_PATH exists."
+fi
+
+# If there's an existing filebrowser.db at $DB_PATH, delete it.
+#
+# This has to be done for new users; if a user already exists in the db,
+# you can't init the db with auth.method noauth. There's also a --noauth parm that
+# can be used when starting filebrowser, and a setting for the same thing, but
+# they have the same problem.
+#
+# Ideally, we should test for an existing db at $DB_PATH, and if it exists use
+# a filebrowser commands to check: 1) if noauth is set, 2) if there's a default
+# user called admin, and 3) if the before_delete command hook is set to
+# "/trash-or-delete.sh \$FILE". If so, we could leave the database intact and reuse it.
+# This would preserve any user filebrowser changes that are saved in the existing db.
+#
+# All three of theses things can be checked using filebrowser commands.
+echo "[init.sh]: Deleting filebrowser db . . ."
+rm -f "$DB_PATH"
+echo "[init.sh]: Filebrowser db  delete result = [$?]"
+
+# Initialize the db with noauth, create a default admin user, and set before_delete command hook.
+if [ ! -f "$DB_PATH" ]; then
+    echo "[init.sh]: Filebrowser.db not found. Initializing Filebrowser database in $DB_DIR_PATH"
+    /filebrowser -d "$DB_PATH" -c "$CONFIG_PATH" config init --auth.method noauth
+    echo "[init.sh]: RC from config init --noauth = [$?]"
+    if [ -x $SETUP_TRASH ]; then
+        echo "[init.sh]: Running $SETUP_TRASH..."
+        "$SETUP_TRASH"
+        echo "[init.sh]: RC from $SETUP_TRASH = [$?]"
+    else
+        echo "[init.sh]: $SETUP_TRASH not found or not executable, skipping..."
+    fi
+    /filebrowser -d "$DB_PATH" -c "$CONFIG_PATH" users add admin admin --perm.admin
+    echo "[init.sh]: RC from users add admin admin = [$?]"
+else
+    echo "[init.sh]: Using existing filebrowser.db located at $DB_PATH..."
+fi
+
+# Expand arguments so $HOME and $USER are forced to resolve
+EXPANDED_ARGS=""
+for arg in "$@"; do
+  EXPANDED_ARGS="$EXPANDED_ARGS $(eval echo "$arg")"
+done
+
+echo "[init.sh]: Running [${EXPANDED_ARGS}]"
+exec sh -c "$EXPANDED_ARGS"
